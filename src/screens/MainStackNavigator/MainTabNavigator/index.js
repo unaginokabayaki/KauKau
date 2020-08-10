a743a37e6e51622c3f9dba644d596c412254b6e8
momonga <kyoukarada@gmail.com>
2020-08-11 02:59:07 +0900

    Add Notification TopTabView

--- src/screens/MainStackNavigator/MainTabNavigator/index.js
+++ src/screens/MainStackNavigator/MainTabNavigator/index.js
@@ -6,9 +6,11 @@ import {
   ScrollView,
   Alert,
   TextInput,
+  Dimensions,
 } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { StateContainer } from 'app/src/AppContext';
 import { Ionicons } from '@expo/vector-icons';
 
@@ -44,6 +46,19 @@ const MainTabNavigator = ({ navigation }) => {
         }}
       />
       <MainTab.Screen
+        name="NotificationStack"
+        component={NotificationStackNavigator}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons
+              name="md-information-circle-outline"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <MainTab.Screen
         name="AccountStack"
         component={AccountStackNavigator}
         options={{
@@ -61,10 +76,31 @@ const NotificationStackNavigator = () => {
   return (
     <NotificationStack.Navigator>
       <NotificationStack.Screen
+        name="NotificationTab"
+        component={TopTabView}
+        // component={NotificationTabNavigator}
+      />
+    </NotificationStack.Navigator>
+  );
+};
+
+const NotificationTab = createMaterialTopTabNavigator();
+const NotificationTabNavigator = () => {
+  return (
+    <NotificationTab.Navigator
+      tabBarOptions={{
+        scrollEnabled: true,
+        indicatorStyle: { backgroundColor: '#e91e63' },
+        // tabStyle: { width: 'auto' },
+      }}
+    >
+      <NotificationTab.Screen
         name="Notification"
         component={NotificationScreen}
       />
-    </NotificationStack.Navigator>
+      <NotificationTab.Screen name="Infomation" component={InformationScreen} />
+      <NotificationTab.Screen name="Todo" component={TodoScreen} />
+    </NotificationTab.Navigator>
   );
 };
 
@@ -73,6 +109,65 @@ const NotificationScreen = () => {
     <View style={styles.container}>
       <Text>NotificationScreen</Text>
     </View>
+  );
+};
+
+const InformationScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>InformationScreen</Text>
+    </View>
+  );
+};
+
+const TodoScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>TodoScreen</Text>
+    </View>
+  );
+};
+
+import { TabView, SceneMap, ScrollPager, TabBar } from 'react-native-tab-view';
+
+const TopTabView = () => {
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'first', title: 'Notification' },
+    { key: 'second', title: 'Information' },
+    { key: 'third', title: 'Todo' },
+  ]);
+
+  const initialLayout = { width: Dimensions.get('window').width };
+
+  const renderScene = SceneMap({
+    first: NotificationScreen,
+    second: InformationScreen,
+    third: TodoScreen,
+  });
+
+  // returnの省略形
+  const renderTabBar = (props) => (
+    <TabBar
+      {...props}
+      indicatorStyle={{ backgroundColor: '#e91e63' }}
+      style={{ backgroundColor: 'white' }}
+      labelStyle={{ color: 'black' }}
+      scrollEnabled={true}
+      // tabStyle={{ width: 140 }}
+      // tabStyle={{ width: 'auto' }}
+    />
+  );
+
+  return (
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={initialLayout}
+      renderPager={(props) => <ScrollPager {...props} />}
+      renderTabBar={renderTabBar}
+    />
   );
 };
 
